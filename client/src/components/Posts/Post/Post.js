2f75c07df13c3291f6a795036b14696547489ba8
wernetke <kvn.wernet@gmail.com>
2021-07-08 22:02:20 -0400

    mern app posty memories

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import useStyles from './styles';
+
+
+const Post = () => {
+
+    const classes = useStyles();
+
+
+    return (
+        <div>
+            <h1>POST</h1>
+        </div>
+    );
+};
+
+export default Post;
